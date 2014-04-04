8aa484eece116a9b4455d4269e635b38241202b3
DaveGall <dlaker74@fullsail.edu>
2014-04-04 14:58:56 -0700

    Just some practice on how to declare and define variables in javascript. Different ways of presenting it and what not to do and what is okay but not recommended.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,5 @@
-var yearBorn = 1955;
-
-/*
-console.log(yearBorn);
-*/
-
-alert(yearBorn);
+var age; //declare on one line
+age = 39; // define on a second line
+//storage container always on the left and the value is on the right
+var yearOfBirth = 1974; // declare and define on the same line
 
