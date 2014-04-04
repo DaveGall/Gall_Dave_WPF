d34bde10983d15cc3d719b1de9f1de338ccd900d
DaveGall <dlaker74@fullsail.edu>
2014-04-04 15:11:09 -0700

    More work on variables, this time we practiced and learned the difference between a string and a boolean variable. We also learned a few good uses of the backslash for telling the computer not to view a symbol as what it might think it is.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,5 +1,10 @@
 var age; //declare on one line
 age = 39; // define on a second line
 //storage container always on the left and the value is on the right
-var yearOfBirth = 1974; // declare and define on the same line
+var isStudent = false; //Boolean Variable
+
+var firstName = "Dave"; //String need quotes
+var phrase = 'I don\'t know \n You\'ll have to ask the teacher';
+//the backslash above makes the computer ignore the apostrophe as ending the quote and the \n puts the next sentence on another line.
+
 
