b0ffcf78c6f6345277db682ba423b5248a9bb9f3
DaveGall <dlaker74@fullsail.edu>
2014-04-04 15:29:55 -0700

    Just more practice, this time on Arrays and how to set them up and how to put them to use.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,10 +1,9 @@
-var age; //declare on one line
-age = 39; // define on a second line
-//storage container always on the left and the value is on the right
-var isStudent = false; //Boolean Variable
+//Arrays
 
-var firstName = "Dave"; //String need quotes
-var phrase = 'I don\'t know \n You\'ll have to ask the teacher';
-//the backslash above makes the computer ignore the apostrophe as ending the quote and the \n puts the next sentence on another line.
+var dogNames = ["Duke", "Jimmy", "Precious", "Dog"];
 
+dogNames[1] = "Duke";
+dogNames[4] = "Killer";
 
+alert (dogNames);
+alert (dogNames[1]);
