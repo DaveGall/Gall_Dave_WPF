1ff1b3ae2413b70751e6571c3f8ac44b60490930
DaveGall <dlaker74@fullsail.edu>
2014-04-05 16:47:27 -0700

    Placing parenthesis in order to tell the computer what to do with a statement or number and also adding symbols into your concatenated string.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,15 @@
-//Concatenating
+//Casting naming a value for an
 
-var a = "6"; //If you want to actually add them you need to take away the ""
-var b = "7";
-var result = a + b;
+var stringVar = "6";
+var result = 7 + Number(stringVar);
 console.log(result);
+
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
+console.log(phoneNo);
 
 
 
