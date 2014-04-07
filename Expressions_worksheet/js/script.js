fe95b8963397a7722f70ec3ef7bfef8145094f84
DaveGall <dlaker74@fullsail.edu>
2014-04-07 14:47:50 -0700

    Average shopping bill; I have just begun adding values on my array for five weeks of shopping.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,11 +1,5 @@
-//Dave Gall, 04/07/14, Expressions Worksheet, Slice of Pie part 2
+//Dave Gall, 04/07/14, Expressions Worksheet, Average shopping bill
 
-var slicesPerPie = 8; //I have assigned a variable for the number of slices per pie.
-var partyPeople = 72; //I have now assigned a variable for the number of party goers.
-var piesOrdered = 17; //I have assigned a value for the amount of pies ordered for the party.
-var sparkyPizza = slicesPerPie * piesOrdered % partyPeople; //I have applied an equation for finding out the amount of pizza Sparky had.
-
-console.log("Sparky got " + (sparkyPizza) + " slices of pizza."); //I added the print function to display the result onto the console.
-
+var myArray = new Array(50, 100, 40, 80, 120); //I have added the variable array for 5 weeks worth of shopping bills.
 
 
