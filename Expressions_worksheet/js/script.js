1d58fae121d815026ba6b427096b4d2a7d699d00
DaveGall <dlaker74@fullsail.edu>
2014-04-07 15:08:19 -0700

    Average shopping bill; I added the equation to give the weekly shopping bill average.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,7 +2,8 @@
 
 var myArray = new Array(50, 100, 40, 80, 120); //I have added the variable array for 5 weeks worth of shopping bills.
 var totalGroceries = myArray[0] + myArray[1] + myArray[2] + myArray[3] + myArray[4];//I have added an equation to add up all the grocery bills.
-console.log(totalGroceries);
+var weeklyAvg = totalGroceries / 5;//I have added an equation to find the weekly average shopping cost
+console.log(weeklyAvg);
 
 
 
