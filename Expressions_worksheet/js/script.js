aa5e881cec227a57b55759cf424127092cfa85eb
DaveGall <dlaker74@fullsail.edu>
2014-04-07 15:00:21 -0700

    Average shopping bill; I have added an equation to add up the total amount of five weeks of grocery bills.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,5 +1,8 @@
 //Dave Gall, 04/07/14, Expressions Worksheet, Average shopping bill
 
 var myArray = new Array(50, 100, 40, 80, 120); //I have added the variable array for 5 weeks worth of shopping bills.
+var totalGroceries = myArray[0] + myArray[1] + myArray[2] + myArray[3] + myArray[4];//I have added an equation to add up all the grocery bills.
+console.log(totalGroceries);
+
 
 
