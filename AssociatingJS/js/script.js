12a62306227f41923068ecb026b73539b8e74724
DaveGall <dlaker74@fullsail.edu>
2014-04-07 16:56:14 -0700

    alert string added to display the results on the screen.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,11 @@
 // Prompt and Alert
 
-var width = prompt("Please enter the width of a rectangle. ");
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width of a rectangle. ");
 var height = prompt("Please enter height of a rectangle. ");
+var area = width * height;
+//console.log(area);
 
-
+alert("The area of your rectangle is " + area + " sq. feet.");
 
 
 
