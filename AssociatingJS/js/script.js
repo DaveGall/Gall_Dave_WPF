8d4b09539e2d5f04622aa5340cb8e6bd877c64e7
DaveGall <dlaker74@fullsail.edu>
2014-04-07 16:46:11 -0700

    Prompts and Alerts first step of creating a prompt. Date of birth.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,6 @@
-//Dog Years
+// Prompt and Alert
 
-var sparkyAge = 2;
-var dogYears = sparkyAge * 7;
-console.log("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years");
+prompt("Enter your year of birth: ");
 
 
 
