c7a005c7b862e8ffc24da3be98e63520dd00a891
DaveGall <dlaker74@fullsail.edu>
2014-04-07 11:31:07 -0700

    Slice of Pie part 1; I have assigned a value for the number of pizzas ordered.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,6 +2,7 @@
 
 var slicesPerPie = 8; //I have assigned a variable for the number of slices per pie.
 var partyPeople = 72; //I have now assigned a variable for the number of party goers.
+var piesOrdered = 18; //I have assigned a value for the amount of pies ordered for the party.
 
 
 
