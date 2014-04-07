ba5df33a98ecabbc7050ad80fb22825e210e8fe2
DaveGall <dlaker74@fullsail.edu>
2014-04-07 11:23:30 -0700

    Slices of Pie part 1; I have assigned a variable for the number of slice there are per pie.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,8 +1,6 @@
-//Dave Gall, 04/07/14, Expressions Worksheet, Dog Years
+//Dave Gall, 04/07/14, Expressions Worksheet, Slice of Pie part 1
 
-var sparkyAge = 2; //Assigning a variable for Sparky the dogs age.
-var dogYears = sparkyAge * 7; //assign an equation for finding Sparky's dog years age
+var slicesPerPie = 8; //I have assigned a variable for the number of slices per pie.
 
-console.log("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years."); //I have created the string for printing the answer to the question onto the console.
 
 
