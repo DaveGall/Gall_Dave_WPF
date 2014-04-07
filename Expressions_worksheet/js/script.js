d4fc3e967fd8b1143e59c535a198c1f343d00d30
DaveGall <dlaker74@fullsail.edu>
2014-04-07 11:39:14 -0700

    Slices of Pie part 1; I have created an equation for finding the number of slices per party goer.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,6 +3,6 @@
 var slicesPerPie = 8; //I have assigned a variable for the number of slices per pie.
 var partyPeople = 72; //I have now assigned a variable for the number of party goers.
 var piesOrdered = 18; //I have assigned a value for the amount of pies ordered for the party.
-
+var slicesPerPerson = slicesPerPie * piesOrdered / parseFloat(partyPeople); //I have applied an equation for finding out the amount of slices per person.
 
 
