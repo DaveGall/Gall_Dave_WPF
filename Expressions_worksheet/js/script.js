234676141fc0364bd6a9ebe9e8e0f1e05875b9f8
DaveGall <dlaker74@fullsail.edu>
2014-04-07 16:26:15 -0700

    Discounts; added the total cost of the item with tax.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -6,4 +6,5 @@ var newTablet = "New Tablet";//Description of item that is on sale.
 var salesTax = 5;//Sales tax of the new tablet.
 
 var withoutTax = originalPrice - (discountPcnt/100 * originalPrice);//cost of the item without tax.
+var withTax = withoutTax + (salesTax/100 * withoutTax);//cost of the item with the 5% tax.
 
