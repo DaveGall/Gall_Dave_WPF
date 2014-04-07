dbf028da0340710ec7de05e7a8b3814f567aed5b
DaveGall <dlaker74@fullsail.edu>
2014-04-07 12:12:56 -0700

    Slices of Pie part 1, I took out a few floating parenthesis, they didn't effect the problem but could cause a problem if someone else was reading the code.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,7 +3,7 @@
 var slicesPerPie = 8; //I have assigned a variable for the number of slices per pie.
 var partyPeople = 72; //I have now assigned a variable for the number of party goers.
 var piesOrdered = 17; //I have assigned a value for the amount of pies ordered for the party.
-var slicesPerPerson = slicesPerPie * piesOrdered / (partyPeople); //I have applied an equation for finding out the amount of slices per person.
+var slicesPerPerson = slicesPerPie * piesOrdered / partyPeople; //I have applied an equation for finding out the amount of slices per person.
 console.log("Each person ate " + parseFloat(slicesPerPerson) + " slices of pizza at the party."); //I added the print function to display the result onto the console.
 
 
