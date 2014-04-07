f1c3cd192c9ea4b9ce28219dbfed9313aa8e3166
DaveGall <dlaker74@fullsail.edu>
2014-04-07 15:13:11 -0700

    Average shopping bill; I have added the console command string to display the sentences and results.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,8 +2,9 @@
 
 var myArray = new Array(50, 100, 40, 80, 120); //I have added the variable array for 5 weeks worth of shopping bills.
 var totalGroceries = myArray[0] + myArray[1] + myArray[2] + myArray[3] + myArray[4];//I have added an equation to add up all the grocery bills.
-var weeklyAvg = totalGroceries / 5;//I have added an equation to find the weekly average shopping cost
-console.log(weeklyAvg);
+var weeklyAvg = totalGroceries / 5;//I have added an equation to find the weekly average shopping cost.
+
+console.log("You have spent a total of " + totalGroceries + " on groceries over 5 weeks. That's an average of " + (weeklyAvg) + " per week.");
 
 
 
