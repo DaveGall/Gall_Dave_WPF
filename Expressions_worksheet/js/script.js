b99f5d2fd9330958f495a7165bea6984fea73983
DaveGall <dlaker74@fullsail.edu>
2014-04-07 11:20:13 -0700

    Dog Years; I have written the console.log string to be printed onto the console for the dog years problem.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,5 +3,6 @@
 var sparkyAge = 2; //Assigning a variable for Sparky the dogs age.
 var dogYears = sparkyAge * 7; //assign an equation for finding Sparky's dog years age
 
+console.log("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years."); //I have created the string for printing the answer to the question onto the console.
 
 
