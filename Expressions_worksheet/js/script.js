f751ad588cd9bd3e640285d2d685af9143b400be
DaveGall <dlaker74@fullsail.edu>
2014-04-07 11:15:49 -0700

    Dog Years; I have assigned a variable/equation to figure out Sparky's age in dog years.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,7 @@
 //Dave Gall, 04/07/14, Expressions Worksheet, Dog Years
 
 var sparkyAge = 2; //Assigning a variable for Sparky the dogs age.
+var dogYears = sparkyAge * 7; //assign an equation for finding Sparky's dog years age
 
 
 
