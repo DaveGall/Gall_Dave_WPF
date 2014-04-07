93fbdb1be1070500246cb2e0d29b05adb812aa26
DaveGall <dlaker74@fullsail.edu>
2014-04-07 10:52:38 -0700

    I worked out the first example on finding the area of a rectangle so that I could practice each step of the worksheet.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,11 @@
+//Dave Gall, 04/07/14, Expressions WorkSheet, Example
+
+
+
+var rectangleWidth = 10;
+var rectangleHeight = 5;
+var rectangleArea = rectangleWidth * rectangleHeight
+
+console.log(rectangleArea);
+
+
