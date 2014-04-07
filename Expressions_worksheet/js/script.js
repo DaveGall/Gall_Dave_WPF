317590fea546977e271ce4f9d3d516537c29c795
DaveGall <dlaker74@fullsail.edu>
2014-04-07 11:00:19 -0700

    updated again because I forgot to label each of the lines explaining their purpose.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,10 +2,9 @@
 
 
 
-var rectangleWidth = 10;
-var rectangleHeight = 5;
-var rectangleArea = rectangleWidth * rectangleHeight
-
-console.log("The area of the rectangle is " + rectangleArea);
+var rectangleWidth = 10; //assigns the width of the rectangle
+var rectangleHeight = 5; //assigns the height of the rectangle
+var rectangleArea = rectangleWidth * rectangleHeight; //Multiplies the width times the height to get the area
+console.log("The area of the rectangle is " + rectangleArea); //prints the result in the console area of the web page
 
 
