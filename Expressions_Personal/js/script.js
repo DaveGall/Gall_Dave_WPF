cf7208eb5bc9b4211e1898540b9bf2c52629e2c8
DaveGall <dlaker74@fullsail.edu>
2014-04-07 17:48:18 -0700

    Wrote the first variable which tells the user what the expected result is supposed to show and asks for the first input.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,2 +1,4 @@
 //Expressions_Personal, David Gall, 04/07/14
 //How many gallons of gas will it take and how much will it cost.
+
+var milesToDrive = prompt("How much gas will you need and how much will it cost you?  \nTo find this out lets start with how many miles you will be driving ")//first variable/prompt to get miles to drive.
