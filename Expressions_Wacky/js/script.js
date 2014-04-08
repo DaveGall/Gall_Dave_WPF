edccfa176016671fd965eb9e4294095a1ec6b4d4
DaveGall <dlaker74@fullsail.edu>
2014-04-08 15:16:33 -0700

    I added the variable asking the user for the width of their garden for my calculation.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,3 +2,4 @@
 //Calculate the amount of garden soil bags needed for your garden.
 
 var rectHeight = prompt("Now that you have built your rectangle garden \nlets see how much dirt you will need. What is the height in feet of your garden?"); //User input variable to add the height.
+var rectWidth = prompt("What is the width in feet of your new garden?");//Another user input variable asking for the width of their garden.
