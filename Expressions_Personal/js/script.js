3efa92e0aae3782cd18c93b7d6b7d2db20e42088
DaveGall <dlaker74@fullsail.edu>
2014-04-07 18:22:05 -0700

    I have taken out size of the gas tank as it isn't relevant to the expected result. I also added an equation that will determine the cost of the trip.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,7 +3,8 @@
 
 var milesToDrive = prompt("How much gas will you need and how much will it cost you?  \nTo find this out lets start with how many miles you will be driving?");//first variable/prompt to get miles to drive.
 var carsMPG = prompt("Next lets get how many miles per gallon your car gets on average?");//another variable to get data from the user for our equation.
-var gasTank = prompt("How many gallons of gas does your car hold?");//Another needed variable that the user will input.
 var costPerGallon = prompt("What is the cost of gas per gallon? Just a numerical amount \nyou don't need a dollar sign");//another variable to get from the user.
 var oneTank = milesToDrive / carsMPG;//Constant for finding how many gallons it will take to reach the destination.
+var tripCost = oneTank * costPerGallon;//Constant for finding out how much the trip will cost.
+
 
