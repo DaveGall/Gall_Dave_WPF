2fe1046bcfd08a313a79ba60d394290aa10134d0
DaveGall <dlaker74@fullsail.edu>
2014-04-08 15:28:54 -0700

    I have added a calculation for finding out how many 1.5 cubic feet garden soil bags the user will need.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,3 +5,4 @@ var rectHeight = prompt("Now that you have built your rectangle garden \nlets se
 var rectWidth = prompt("What is the width in feet of your new garden?");//Another user input variable asking for the width of their garden.
 var rectLength = prompt("Now the last measurement we need for your garden is the length in feet?");//Variable asking for the length of the garden input.
 var rectVolume = rectHeight * rectWidth * rectLength * (80/100);//This calculation will give me and the user the amount of dirt needed to fill their garden 80%.
+var bagsNeeded = rectVolume / 1.5;//This calculation will divide the volume needed by 1.5 which is the standard garden soil bag.
