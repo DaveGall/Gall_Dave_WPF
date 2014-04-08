64663898f87df91347318c14cae6f10c47423ecd
DaveGall <dlaker74@fullsail.edu>
2014-04-08 15:41:04 -0700

    I added a calculation that will show how many trips to and from the car you will take if you carry two bags at a time. This includes the store to the car and the car to the garden.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,3 +8,4 @@ var rectVolume = rectHeight * rectWidth * rectLength * (80/100);//This calculati
 var bagsNeeded = rectVolume / 1.5;//This calculation will divide the volume needed by 1.5 which is the standard garden soil bag.
 var yardsOfSoil = rectVolume / 27;//This calculation changes the cubic feet into cubic yards in case the user needs a different measurement.
 var soilWeight = rectVolume * 35;//This calculation is meant to tell the user how much the total amount of soil needed will weigh. Soil varies depending on several factors but this was a rounded weight.
+var carTrips = bagsNeeded;//Just a little calculation on how many trips it will take carrying two bags at a time. I left it as the initial value because you need to load and unload the car.
