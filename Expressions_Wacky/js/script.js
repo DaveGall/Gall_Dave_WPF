1ce45ba50cfd35c26bb73f49d21858bd8ebea9a7
DaveGall <dlaker74@fullsail.edu>
2014-04-08 15:14:32 -0700

    I added my first variable asking for input from the user for the height of their garden.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,4 @@
 //David Gall, 04/07/14, Expressions_Wacky
 //Calculate the amount of garden soil bags needed for your garden.
 
+var rectHeight = prompt("Now that you have built your rectangle garden \nlets see how much dirt you will need. What is the height in feet of your garden?"); //User input variable to add the height.
