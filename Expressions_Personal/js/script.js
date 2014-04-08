40b23c8adeadbc07afd3502226d22fb2f3c27aa1
DaveGall <dlaker74@fullsail.edu>
2014-04-07 17:54:42 -0700

    Added a prompt to find out how many miles per gallon their car acheives.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,5 @@
 //Expressions_Personal, David Gall, 04/07/14
 //How many gallons of gas will it take and how much will it cost.
 
-var milesToDrive = prompt("How much gas will you need and how much will it cost you?  \nTo find this out lets start with how many miles you will be driving ")//first variable/prompt to get miles to drive.
+var milesToDrive = prompt("How much gas will you need and how much will it cost you?  \nTo find this out lets start with how many miles you will be driving?");//first variable/prompt to get miles to drive.
+var carsMPG = prompt("Next lets get how many miles per gallon your car gets on average?");//another variable to get data from the user for our equation.
