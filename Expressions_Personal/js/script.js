c5cc9c13b8016a78fb08ce07433556bf38d2a039
DaveGall <dlaker74@fullsail.edu>
2014-04-07 18:31:20 -0700

    Changed the one tank constant to gallonsNeeded as it is more relevant and less confusing to someone who is reading the code.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,7 +4,8 @@
 var milesToDrive = prompt("How much gas will you need and how much will it cost you?  \nTo find this out lets start with how many miles you will be driving?");//first variable/prompt to get miles to drive.
 var carsMPG = prompt("Next lets get how many miles per gallon your car gets on average?");//another variable to get data from the user for our equation.
 var costPerGallon = prompt("What is the cost of gas per gallon? Just a numerical amount \nyou don't need a dollar sign");//another variable to get from the user.
-var oneTank = milesToDrive / carsMPG;//Constant for finding how many gallons it will take to reach the destination.
-var tripCost = oneTank * costPerGallon;//Constant for finding out how much the trip will cost.
+var gallonsNeeded = milesToDrive / carsMPG;//Constant for finding how many gallons it will take to reach the destination.
+var tripCost = gallonsNeeded * costPerGallon;//Constant for finding out how much the trip will cost.
+console.log("You will use " + )
 
 
