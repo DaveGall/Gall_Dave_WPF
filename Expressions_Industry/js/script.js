91a44d89219912f814bd39a6db2a7846b65c4ad2
DaveGall <dlaker74@fullsail.edu>
2014-04-08 22:26:07 -0700

    Added a user input variable asking for their first name.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,4 @@
 //David Gall, 04/07/14, Industry Expression
 //This project will be about my industry but I have no idea what I am going to calculate yet.
 
+var firstName = prompt("Please enter your first name.");//I am asking for an input value from the user.
