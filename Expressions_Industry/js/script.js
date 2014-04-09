2453d34f1219abb0230fb30054e0130a19221a8f
DaveGall <dlaker74@fullsail.edu>
2014-04-08 22:27:49 -0700

    Another prompt this time asking for the users last name to be input.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,3 +2,4 @@
 //This project will be about my industry but I have no idea what I am going to calculate yet.
 
 var firstName = prompt("Please enter your first name.");//I am asking for an input value from the user.
+var lastName = prompt("Please enter your last name.");//I am asking for the users last name.
