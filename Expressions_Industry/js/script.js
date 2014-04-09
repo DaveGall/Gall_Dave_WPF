be39f0df1fd6c2c3013eeef5c54136a99afe6ca6
DaveGall <dlaker74@fullsail.edu>
2014-04-08 22:31:33 -0700

    I have added another variable for user input asking for the state in which the company is.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,3 +4,4 @@
 var firstName = prompt("Please enter your first name.");//I am asking for an input value from the user.
 var lastName = prompt("Please enter your last name.");//I am asking for the users last name.
 var company = prompt("Please enter the company you work for.");//I am asking for the users company name that they work for.
+var state = prompt("Please enter the state where your businesses home of operations is.");//Asking here for the name of the state the business resides.
