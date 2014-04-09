21d802d4638421c70624d662566f7480ec68230c
DaveGall <dlaker74@fullsail.edu>
2014-04-09 16:53:25 -0700

    trying to put things where they go

---
+++ Gall_Dave_WPF/Expressions_Industry/js/script.js
@@ -0,0 +1,12 @@
+//David Gall, 04/07/14, Industry Expression
+//This project will be about my industry but I have no idea what I am going to calculate yet.
+
+var firstName = prompt("Please enter your first name.");//I am asking for an input value from the user.
+var lastName = prompt("Please enter your last name.");//I am asking for the users last name.
+var company = prompt("Please enter the company you work for.");//I am asking for the users company name that they work for.
+var state = prompt("Please enter the state where your businesses home of operations is.");//Asking here for the name of the state the business resides.
+
+
+
+
+//console.log(firstName + " " + lastName + "\n" + company + "\n" + state);
