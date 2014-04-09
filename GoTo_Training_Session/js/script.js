9c361e3580a37bee6f88699a8544d2323c8242b7
DaveGall <dlaker74@fullsail.edu>
2014-04-09 15:14:28 -0700

    added a comment about what I was looking for and then a user input variable asking for their name.

--- GoTo_Training_Session/js/script.js
+++ GoTo_Training_Session/js/script.js
@@ -1,3 +1,7 @@
 //GoTo Training session.
 //Figure out how old we currently are and how old we will be
 //in a given amount of years.
+
+//Prompt user for name.
+
+var name = prompt("Please type in your name.");
