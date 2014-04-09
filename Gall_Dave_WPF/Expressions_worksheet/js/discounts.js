f09632edb97844ffa632d520542582b2accbde1e
DaveGall <dlaker74@fullsail.edu>
2014-04-09 16:54:50 -0700

    trying to put things where they go

---
+++ Gall_Dave_WPF/Expressions_worksheet/js/discounts.js
@@ -0,0 +1,11 @@
+//Dave Gall, 04/07/14, Expressions Worksheet, Discounts
+
+var originalPrice = 300;//Original price of the item.
+var discountPcnt = 10;//discount of the item 10%.
+var newTablet = "New Tablet";//Description of item that is on sale.
+var salesTax = 5;//Sales tax of the new tablet.
+
+var withoutTax = originalPrice - (discountPcnt/100 * originalPrice);//cost of the item without tax.
+var withTax = withoutTax + (salesTax/100 * withoutTax);//cost of the item with the 5% tax.
+console.log("Your " + newTablet + " was originally $" + originalPrice + " but after a " + discountPcnt + "% discount, it is now $" + withoutTax + " without tax, and $" + withTax + " with tax.");
+
---
+++ Gall_Dave_WPF/Expressions_worksheet/js/slice_of_pie_part2.js
@@ -0,0 +1,11 @@
+//Dave Gall, 04/07/14, Expressions Worksheet, Slice of Pie part 2
+
+var slicesPerPie = 8; //I have assigned a variable for the number of slices per pie.
+var partyPeople = 72; //I have now assigned a variable for the number of party goers.
+var piesOrdered = 17; //I have assigned a value for the amount of pies ordered for the party.
+var sparkyPizza = slicesPerPie * piesOrdered % partyPeople; //I have applied an equation for finding out the amount of pizza Sparky had.
+
+console.log("Sparky got " + (sparkyPizza) + " slices of pizza."); //I added the print function to display the result onto the console.
+
+
+
