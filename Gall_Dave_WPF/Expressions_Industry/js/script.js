46798877bef528e2d1eb23b6c944ab7774796622
DaveGall <dlaker74@fullsail.edu>
2014-04-09 18:46:43 -0700

    I have added the 20 year calculation to my console.log and alert string for displaying the results to the user.

--- Gall_Dave_WPF/Expressions_Industry/js/script.js
+++ Gall_Dave_WPF/Expressions_Industry/js/script.js
@@ -22,10 +22,11 @@ userInformation[3] = ("Weekly earnings: $"+weeklyEarnings);//This will give the 
 userInformation[4] = ("Monthly earnings: $"+monthlyEarnings);//Displays the results of the calculation of monthly earnings.
 userInformation[5] = ("Yearly earnings: $"+yearlyEarnings);//Displays the users yearly earnings.
 userInformation[6] = ("In 10 years you will earn: $"+tenMoreYears);//Displays the result of the calculation for what you can make in ten years.
-userInformation[7] = ("In 15 years you will make: $"+fifteenYears);//A place setting for 15 years of income.
+userInformation[7] = ("In 15 years you will have made: $"+fifteenYears);//A place setting for 15 years of income.
+userInformation[8] = ("In 20 years you will have made: $"+twentyYears);//Calc. for twenty years of work.
 
-alert(userInformation[0] +"\n"+userInformation[1]+"\n"+userInformation[2]+"\n"+userInformation[3]+"\n"+userInformation[4]+"\n"+userInformation[5]+"\n"+userInformation[6]+"\n"+userInformation[7]);
-console.log(userInformation[0] +"\n"+userInformation[1]+"\n"+userInformation[2]+"\n"+userInformation[3]+"\n"+userInformation[4]+"\n"+userInformation[5]+"\n"+userInformation[6]+"\n"+userInformation[7]);
+alert(userInformation[0] +"\n"+userInformation[1]+"\n"+userInformation[2]+"\n"+userInformation[3]+"\n"+userInformation[4]+"\n"+userInformation[5]+"\n"+userInformation[6]+"\n"+userInformation[7]+"\n"+userInformation[8]);
+console.log(userInformation[0] +"\n"+userInformation[1]+"\n"+userInformation[2]+"\n"+userInformation[3]+"\n"+userInformation[4]+"\n"+userInformation[5]+"\n"+userInformation[6]+"\n"+userInformation[7]+"\n"+userInformation[8]);
 //var company = prompt("Please enter the company you work for.");//I am asking for the users company name that they work for.
 //var state = prompt("Please enter the state where your businesses home of operations is.");//Asking here for the name of the state the business resides.
 
