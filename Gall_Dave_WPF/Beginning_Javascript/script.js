15566fafc65285adf81ce365c9e9cf5266078fe8
DaveGall <dlaker74@fullsail.edu>
2014-04-10 21:40:27 -0700

    Added an if statement to decide when to display a certain text.

--- Gall_Dave_WPF/Beginning_Javascript/script.js
+++ Gall_Dave_WPF/Beginning_Javascript/script.js
@@ -4,4 +4,14 @@ var degCent;
 
 degCent = 5/9 * (degFahren - 32);
 
+alert(parseFloat(degFahren).toFixed(2)+"\xB0 Fahrenheit is "+parseFloat(degCent).toFixed(2)+"\xB0 centigrade\n");
+console.log(degFahren+"\xB0 Fahrenheit is "+parseFloat(degCent).toFixed(2)+"\xB0 centigrade\n");
+
+if (degCent < 0){
+    console.log("That's below the freezing point of water!");
+}
+if(degCent === 100)
+    console.log("That's the boiling point of water!");
+
+
 
