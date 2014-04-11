e8c593f72a100824edc4c39d8cdd2cbe268f2afe
DaveGall <dlaker74@fullsail.edu>
2014-04-10 18:00:00 -0700

    Adding an else if statement to our equation to give a third response/option.

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -1,14 +1,18 @@
-// Conditional logic
+// Conditional logic - else if
 
 var kidHeight = 40;
 var minHeight = 48;
-
+var wParentHeight = 45; //the height of the kid with the parent.
 //If the child is old enough print to the console you can ride.
 //If the kid is over 48 inches in height.
 if(kidHeight > minHeight){
     //code performed if condition is true.
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //You can ride with a parent.
+    console.log("You can ride but only with a parent present.");
 }else{
+    //Sorry you got some growin' to do.
     console.log("Sorry kid you got some growing to do!");
 }
 
