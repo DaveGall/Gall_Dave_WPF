1e80ba68ab070d596c56b384fb52ea5e7005c207
DaveGall <dlaker74@fullsail.edu>
2014-04-10 17:51:07 -0700

    Two if statements to give a result if one wasn't true then the other would display.

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -2,12 +2,15 @@
 
 var kidHeight = 40;
 var minHeight = 48;
-var sneakerLift = 2;
+
 //If the child is old enough print to the console you can ride.
 //If the kid is over 48 inches in height.
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true.
     console.log("You can ride the coaster!");
+}
+if(kidHeight <= minHeight){
+    console.log("Sorry kid you got some growing to do!");
 }
 
 
