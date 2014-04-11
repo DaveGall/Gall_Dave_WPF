04ad2f82c45b484d2af557715f546e2ba42d25a8
DaveGall <dlaker74@fullsail.edu>
2014-04-10 17:45:17 -0700

    Compared two variables to get the desired result.kidHeight/minHeight

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -1,10 +1,10 @@
 // Conditional logic
 
 var kidHeight = 30;
-
+var minHeight = 48;
 //If the child is old enough print to the console you can ride.
 //If the kid is over 48 inches in height.
-if(kidHeight > 48){
+if(kidHeight > minHeight){
     //code performed if condition is true.
     console.log("You can ride the coaster!");
 }
