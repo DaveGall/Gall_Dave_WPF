d528704439dceae4c4ca4d410f3bb6fdfd62085d
DaveGall <dlaker74@fullsail.edu>
2014-04-11 11:50:29 -0700

    I have added an if statement for true, the competitor can compete if they are greater than or equal to 250 pounds.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -3,3 +3,8 @@
 //The competitors weight determines if tey get in or not.
 //Ask for the competitors weight.
 var compWeight = Number(prompt("What is your weight?"));
+
+if(compWeight >= 250){
+    //If their weight is greater than or equaled to 250 this will be the result.
+    console.log("The competitor qualifies for the heavyweight division.");
+}
