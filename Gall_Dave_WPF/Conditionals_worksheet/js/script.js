08173ede73a28daf96f02de823fc1667e402470c
DaveGall <dlaker74@fullsail.edu>
2014-04-11 12:24:02 -0700

    I have added two equations to calculate the temp for celsius and fahrenheit. hopefully these are correct.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -2,3 +2,5 @@
 //Celsius to Fahrenheit conversion
 //first I am going to ask for a temperature to convert from the user.
 var temp = prompt("Type in the temperature you would like to convert to Celsius or Fahrenheit.");
+var f = temp * 9/5+32;//If the temp is in celsius.
+var c = 5/9 * (temp - 32);//If the temp is in fahrenheit.
