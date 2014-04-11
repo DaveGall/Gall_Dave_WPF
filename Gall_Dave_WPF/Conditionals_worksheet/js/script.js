853bc8acc702af6b8efd18cf1a8fafbba7268817
DaveGall <dlaker74@fullsail.edu>
2014-04-11 16:08:16 -0700

    I have created an if statement that will display results in celsius when the input value is degrees fahrenheit.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -1,6 +1,10 @@
 //David Gall, 04/11/14, Conditionals worksheet.
 //Celsius to Fahrenheit conversion
 //first I am going to ask for a temperature to convert from the user.
-var temp = prompt("Type in the temperature you would like to convert to Celsius or Fahrenheit.");
-var f = temp * 9/5+32;//If the temp is in celsius.
-var c = 5/9 * (temp - 32);//If the temp is in fahrenheit.
+var degrees = parseInt(prompt("Type in the temperature you would like to convert to Celsius or Fahrenheit."));
+// f = degrees * 9/5 + 32;//If the temp is in celsius.
+// c = 5/9 * (degrees - 32);//If the temp is in fahrenheit.
+//Create a statement that will display the results of the fahrenheit temperature entered into degrees celsius.
+if(Number(degrees) && "f"){
+    console.log(5/9 * (degrees - 32));
+}
