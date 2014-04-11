f8f1690fa389c038fd3e04a2a7912971d5ec9806
DaveGall <dlaker74@fullsail.edu>
2014-04-10 17:47:44 -0700

    Added sneaker lift to the expressions to add to the kids height so he could be taller than his actual height.

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -1,10 +1,11 @@
 // Conditional logic
 
-var kidHeight = 30;
+var kidHeight = 40;
 var minHeight = 48;
+var sneakerLift = 2;
 //If the child is old enough print to the console you can ride.
 //If the kid is over 48 inches in height.
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true.
     console.log("You can ride the coaster!");
 }
