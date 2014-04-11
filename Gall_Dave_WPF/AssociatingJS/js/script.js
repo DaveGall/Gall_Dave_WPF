c9cb76db43528326d497c294faa5f57e76abcd13
DaveGall <dlaker74@fullsail.edu>
2014-04-10 17:22:45 -0700

    if and else conditional statemnets lesson.

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -1,12 +1,11 @@
-// Prompt and Alert
+// Conditional Statement using if(){
+//                               }
 
-var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width of a rectangle. ");
-var height = prompt("Please enter height of a rectangle. ");
-var area = width * height;
-//console.log(area);
-
-alert("The area of your rectangle is " + area + " sq. feet.");
-
+if(sunny) {
+    goToTheBeach();
+}else{
+    goToTheMovies();
+}
 
 
 
