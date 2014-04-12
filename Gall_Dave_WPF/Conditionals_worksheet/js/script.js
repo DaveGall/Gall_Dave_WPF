24f5a97d5f8edba99ee5e63709ecaa5d12c888ba
DaveGall <dlaker74@fullsail.edu>
2014-04-12 10:57:09 -0700

    I have just started the Check login information script. First varaiable is a prompt for the username of the user.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -1,37 +1,7 @@
 //David Gall, 04/11/14, Conditionals worksheet.
-//Grade Letter Calculator.
-
-//Ask for user to input their score for the class.
-var classScore = prompt("What score did you receive for this class?");
-//Calculation to determine the students final score in the class.
-var grade = classScore+"%";
-//I am creating the if true statements for each possibility.
-if(classScore >= 95 && classScore <= 100) {
-    console.log("You have a " + grade + " which means you have an A+ in the class.");//A+ response
-}
-if(classScore >= 90 && classScore <= 94){
-        console.log("You have a "+grade+" which means you have an A in the class.");//A response
-}
-if(classScore >=85 && classScore <= 89){
-    console.log("You have a "+grade+" which means you have a B+ in the class.");//B+ response
-}
-if(classScore >= 80 && classScore <= 84){
-    console.log("You have a "+grade+" which means you have a B in the class.");//B response
-}
-if(classScore >= 76 && classScore <= 79){
-    console.log("You have a "+grade+" which means you have a C+ in the class.");//C+ response
-}
-if(classScore >= 73 && classScore <= 75){
-    console.log("You have a "+grade+" which means you have a C in the class.");//C response
-}
-if(classScore >= 70 && classScore <= 72){
-    console.log("You have a "+grade+" which means you have a D in the class.");//D response
-}
-if(classScore >= 0 && classScore <= 69){
-    console.log("You have a "+grade+" which means you have an F in the class.");//F response
-}else{
-    console.log("Please try again");
-}
+//Check Login problem.
+//A prompt to retrieve the users username.
+var userName = prompt("Please enter your username.");
 
 
 
@@ -51,7 +21,3 @@ if(classScore >= 0 && classScore <= 69){
 
 
 
-/*A+95-1004.0A90-943.5B+85-893.0B80-842.5C+76-792.0C73-751.5D70-721.0F0-690.0
-GRADUATE
-
-A+95-1004.0A90-943.5B+85-893.0B80-842.5C73-792.0D70-721.0F0-690.0*/
