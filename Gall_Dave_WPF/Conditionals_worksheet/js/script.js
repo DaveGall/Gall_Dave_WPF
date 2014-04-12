7e0bfda1aa12f674bd0adc62c235e6df6557a67a
DaveGall <dlaker74@fullsail.edu>
2014-04-12 11:13:08 -0700

    I have made the first if/else statement. I have made the username print out the desired result, however I think I need to adjust that because I want both to match.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -7,7 +7,12 @@ var password = prompt("Please enter your password.");
 //inputting the correct username and password for the computer to check against.
 var name = "Dave Gall";
 var pass = "Princess1";
-
+//I am starting my if/else statement to determine if the username and password are correct.
+if(userName === "Dave Gall"){
+    console.log("Welcome "+name+ "!");
+}else{
+    console.log("User not found try again.");
+}
 
 
 
