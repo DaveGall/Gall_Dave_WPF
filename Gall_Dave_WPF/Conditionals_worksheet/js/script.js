c795273eaf530d657dec00940b7a18a409a8e467
DaveGall <dlaker74@fullsail.edu>
2014-04-11 18:20:24 -0700

    I will come back to this one at the end of the assignment. It is giving me a bit of trouble and I want to make sure I get my assignment done. I will definitely do this one because I would like to learn how to do it. I know how to calculate everything I just don't know how to get my code to recognize the letter.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -2,10 +2,11 @@
 //Celsius to Fahrenheit conversion
 //first I am going to ask for a temperature to convert from the user.
 var degrees = parseInt(prompt("Type in the temperature you would like to convert to Celsius or Fahrenheit."));
-// f = degrees * 9/5 + 32;//If the temp is in celsius.
-// c = 5/9 * (degrees - 32);//If the temp is in fahrenheit.
+var f = "f";
+var c = "c";
 //Create a statement that will display the results of the fahrenheit temperature entered into degrees celsius.
-if(Number(degrees) && "f"){
-    console.log("The temperature is "+5/9 * (degrees - 32)+"\xB0 celsius.");//If the input is a number and f then the output will be converted into celsius.
-}else
-    console.log(degrees * 9/5 + 32);//If statement that will convert the celsius to fahrenheit.
+if(Number(degrees) && f){
+    console.log("The temperature is "+parseFloat(5/9 * (degrees - 32)).toFixed(2)+"\xB0 celsius.");//If the input is a number and f then the output will be converted into celsius.
+}else if(Number(degrees) && c) {
+    console.log(degrees * 9 / 5 + 32);//If statement that will convert the celsius to fahrenheit.
+}
