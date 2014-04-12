755ccdcf86327d81bc8a30dd5b96461d9036b908
DaveGall <dlaker74@fullsail.edu>
2014-04-11 18:25:04 -0700

    I am starting the miles left to travel problem. I have added two user prompts for miles per gallon for their car and percent left in tank.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -1,12 +1,7 @@
 //David Gall, 04/11/14, Conditionals worksheet.
-//Celsius to Fahrenheit conversion
-//first I am going to ask for a temperature to convert from the user.
-var degrees = parseInt(prompt("Type in the temperature you would like to convert to Celsius or Fahrenheit."));
-var f = "f";
-var c = "c";
-//Create a statement that will display the results of the fahrenheit temperature entered into degrees celsius.
-if(Number(degrees) && f){
-    console.log("The temperature is "+parseFloat(5/9 * (degrees - 32)).toFixed(2)+"\xB0 celsius.");//If the input is a number and f then the output will be converted into celsius.
-}else if(Number(degrees) && c) {
-    console.log(degrees * 9 / 5 + 32);//If statement that will convert the celsius to fahrenheit.
-}
+//Last chance for gas.
+
+//user enter the MPG for their car.
+var mpg = prompt("What is your cars miles per gallon?");
+//Have the user enter how much gas they have left.
+var prcntLeft = prompt("How much gas do you have left in your tank?");
