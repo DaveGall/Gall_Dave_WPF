219124d2a12ed350a9e47aedfdc9ab14138c52fa
DaveGall <dlaker74@fullsail.edu>
2014-04-12 11:38:13 -0700

    My last variable prompt of the right driver side tire pressure was added.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -6,6 +6,8 @@ var lfTire = prompt("What is the pressure of your front driver side tire?");
 var rfTire = prompt("What is the pressure of your front passenger side tire?");
 //Asking for the pressure of the rear passenger side tire.
 var rrTire = prompt("What is the pressure of your rear passenger tire?");
+//Asking for my final variable of the rear driver side tires pressure.
+var rlTire = prompt("What is the tire pressure of your rear driver side tire?");
 
 
 
