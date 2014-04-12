b7de4af1f82f97c7b9496ad5a24929bc9d64889a
DaveGall <dlaker74@fullsail.edu>
2014-04-11 18:27:43 -0700

    This equation will give me the decimal equivalent of the percentage of gas left in the tank.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -5,3 +5,7 @@
 var mpg = prompt("What is your cars miles per gallon?");
 //Have the user enter how much gas they have left.
 var prcntLeft = prompt("How much gas do you have left in your tank?");
+//Have the user input the size of their gas tank.
+var tankCapacity = prompt("How many gallons of gas does your tank hold?");
+//Calculation for the percentage of gas left in the tank.
+var tankPrcnt = prcntLeft/100;
