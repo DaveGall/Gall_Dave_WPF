6ab2001ede772705d5214eb71c0d15709e0bac38
DaveGall <dlaker74@fullsail.edu>
2014-04-12 11:33:08 -0700

    First variable prompt asking for the left front tire pressure.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -1,6 +1,7 @@
 //David Gall, 04/11/14, Conditionals worksheet.
 //Tire Pressure 1
-
+//Asking for variables that can be input into my Array.
+var lfTire = prompt("What is the pressure of your front driver side tire?");
 
 
 
