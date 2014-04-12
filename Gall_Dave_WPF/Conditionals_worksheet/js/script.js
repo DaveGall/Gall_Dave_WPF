fbad1151e29525b3e45488507cebed8597a79898
DaveGall <dlaker74@fullsail.edu>
2014-04-12 11:34:41 -0700

    A variable asking for the passenger side front tire.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -2,6 +2,9 @@
 //Tire Pressure 1
 //Asking for variables that can be input into my Array.
 var lfTire = prompt("What is the pressure of your front driver side tire?");
+//Asking for the pressure of the passenger side front tire.
+var rfTire = prompt("What is the pressure of your front passenger side tire?");
+
 
 
 
