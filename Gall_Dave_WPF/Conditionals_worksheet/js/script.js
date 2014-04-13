8e887a1c6bd5b689d2299bb55473749f4aaa23cf
DaveGall <dlaker74@fullsail.edu>
2014-04-12 17:10:37 -0700

    Added a comment for the if true condition.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -5,7 +5,7 @@ var movieTime = prompt("What time does the movie start?");
 //I will now set up the variable for the users age.
 var customerAge = prompt("How old are you?");
 //I am creating the if/else statement to get the desired results.
-if(movieTime >= 3 && movieTime <=5 || customerAge >=55 && customerAge <=10){
+if(movieTime >= 3 && movieTime <=5 || customerAge >=55 && customerAge <=10){//This if statement accounts for two different variables that will give the same result. 
     console.log("The ticket price is $7.00.");
 }else{
     console.log("The ticket price is $12.00.");
