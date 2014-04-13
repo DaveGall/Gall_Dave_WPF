0715c8371350735e6ca59486619b5f4e661c6eef
DaveGall <dlaker74@fullsail.edu>
2014-04-12 19:30:30 -0700

    Temperature Converter; Final script, I have added parseFloat().toFixed(2) to keep my decimal to two places instead of running decimals.

--- Gall_Dave_WPF/Conditionals_worksheet/js/script.js
+++ Gall_Dave_WPF/Conditionals_worksheet/js/script.js
@@ -6,11 +6,11 @@ var f = 5/9 * (degree - 32);//calculation for fahrenheit to celsius.
 var c = degree * 9/5 + 32;//calculation for celsius to fahrenheit.
 //if condition for the number and f.
 if(farCel === "f"){
-    console.log("The temperature is "+ f +"\xB0 Celsius.");
+    console.log("The temperature is "+ parseFloat(f).toFixed(2) +"\xB0 Celsius.");
 }if(farCel === "c"){
-    console.log("The temperature is "+ c +"\xB0 Fahrenheit.");
+    console.log("The temperature is "+ parseFloat(c).toFixed(2) +"\xB0 Fahrenheit.");
 }
-
+//Added parseFloat().toFixed(2) to make sure my answers were correct and not running decimals.
 
 
 
