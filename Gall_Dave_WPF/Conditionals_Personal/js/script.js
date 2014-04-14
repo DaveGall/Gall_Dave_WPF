a1dae2e52010c925162654b391489a70b1b47957
DaveGall <dlaker74@fullsail.edu>
2014-04-14 14:08:37 -0700

    I have added the conditions for getting the correct mode of transportation from the user. By drivin, plane or train and the number of tickets and cost.

--- Gall_Dave_WPF/Conditionals_Personal/js/script.js
+++ Gall_Dave_WPF/Conditionals_Personal/js/script.js
@@ -9,11 +9,19 @@ var driving = prompt("Are you driving? (y) for yes or (n) for no.");
 //This next series will give the correct response to the answer of the previous question.
 if(driving === "y"){
     var milesToDrive = prompt("How many miles away is your destination?");
+}if(driving === "n"){
+    //Find out which mode of transportation they are taking. Flying yes or no.
+    var flying = prompt("Are you flying? (y) for yes or (n) for no.");
+}if(flying === "y"){
+    var numberOfTickets = prompt("How many plane tickets did you buy?");
+    var priceOfEach = prompt("How much was each plane ticket? \nNo need for any symbols just numbers is good.");
+}if(flying === "n"){
+    //Find out which mode of transportation they are taking. Train yes or no.
+    var train = prompt("Are you traveling by train? (y) for yes or (n) for no.");
+}if(train === "y"){
+    var numberOfTrainTickets = prompt("How many train tickets do you need to purchase?");
+    var costPerTrainTicket = prompt("How much does each train ticket cost? \nNo need for any symbols just numbers is good.");
 }
-//Find out which mode of transportation they are taking. Flying yes or no.
-var flying = prompt("Are you flying? (y) for yes or (n) for no.");
-//Find out which mode of transportation they are taking. Train yes or no.
-var train = prompt("Are you traveling by train? (y) for yes or (n) for no.");
 
 
 //If there is no value in savings then this will prompt the user to enter valid data.
