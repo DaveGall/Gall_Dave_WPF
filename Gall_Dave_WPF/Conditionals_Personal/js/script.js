ea6c496f4e37a1c2c106c6bb25dbe9acd738e948
DaveGall <dlaker74@fullsail.edu>
2014-04-14 15:19:19 -0700

    I have rearranged my code so it was easier to read.

--- Gall_Dave_WPF/Conditionals_Personal/js/script.js
+++ Gall_Dave_WPF/Conditionals_Personal/js/script.js
@@ -1,20 +1,41 @@
 //David Gall, 04/14/14, Conditionals_Personal
-//How long can we vacation for?
+//How much will your vacation cost your family and can you afford it?
 //Prompt to ask how much you have saved.
 var savings = prompt("How much money do you have saved? \nJust a number please, you don't need any dollar signs.");
+//If there is no value in savings then this will prompt the user to enter valid data.
+if(savings === ""){
+    prompt("Oops you forgot to write in a number. Please try again.");
+}
+//Get the length of the vacation from the user.
+var duration = prompt("How many days is your trip planned for?");
+//Tests the duration prompt to make sure there is a response.
+if(duration === ""){
+    prompt("Oops you forgot to write in a number. Please try again.");
+}
 //This prompt is to get the cost of the hotel per night.
 var hotel = prompt("How much does the hotel cost per night? \nJust a number please, you don't need any dollar signs.", "");
+//If there is no value for hotel then this prompt will appear to ensure data is input.
+if(hotel === ""){
+    prompt("Oops you forgot to write in a number. Please try again.");
+}
 //Find out which mode of transportation they are taking. Driving yes or no.
 var driving = prompt("Are you driving? (y) for yes or (n) for no.");
+//Tests the driving prompt for a response.
+if(driving === ""){
+    prompt("Oops you forgot something. Please enter a valid response.");
+}
 //This next series will give the correct response to the answer of the previous question.
 if(driving === "y" || driving === "Y"){//if the response is yes then do this.
     var milesToDrive = prompt("How many miles away is your destination?");//Distance that is needed to calculate
     var priceOfGas = prompt("What is the average price of gas per gallon? \nNo symbols are needed just a umber.");//variable on price of gas per gallon.
     var milesPerGallon = prompt("What is your cars average miles per gallon?");//Getting information on the cars miles per gallon.
-}if(driving === "n" || driving === "N"){//If the response is no then do this.
+}if(driving === "n" || driving === "N") {//If the response is no then do this.
     //Find out which mode of transportation they are taking. Flying yes or no.
     var flying = prompt("Are you flying? (y) for yes or (n) for no.");//A no from the previous question will land you here.
-}if(flying === "y" || flying === "Y"){//A yes to this question will produce the results below.
+//Tests the flying prompt for valid input
+}if(flying === ""){
+        prompt("Oops you forgot something. Please enter a valid response.");
+    }else if(flying === "y" || flying === "Y"){//A yes to this question will produce the results below.
     var numberOfTickets = prompt("How many plane tickets did you buy?");//How many plane tickets they will need.
     var priceOfEach = prompt("How much was each plane ticket? \nNo need for any symbols just numbers is good.");//The cost of each plane ticket.
     var needARental = prompt("Will you need a rental car? (y) for yes and (n) for no.");//If a rental car is needed.
@@ -25,6 +46,10 @@ if(driving === "y" || driving === "Y"){//if the response is yes then do this.
 }if(flying === "n" || flying === "N"){//A no response to the previous question will land you here.
     //Find out which mode of transportation they are taking. Train yes or no.
     var train = prompt("Are you traveling by train? (y) for yes or (n) for no.");
+//Tests the train prompt for input.
+    if(train === ""){
+        prompt("Oops you forgot something. Please enter a valid response.");
+    }
 }if(train === "y" || train === "Y"){//This is the response if you choose to travel by train.
     var numberOfTrainTickets = prompt("How many train tickets do you need to purchase?");//How many train tickets are needed.
     var costPerTrainTicket = prompt("How much does each train ticket cost? \nNo need for any symbols just numbers is good.");//Cost of each ticket.
@@ -34,28 +59,7 @@ if(driving === "y" || driving === "Y"){//if the response is yes then do this.
         var rentalCost = prompt("How much does the rental car cost per day? \nNo need for signs just a number will work.");//Cost per day of the rental.
     }
     //The next statement is in case they aren't traveling by train. At this point I have no other options for you sorry.
-}else{
-    alert("Sorry I have no other options for you at this time. Please check back in the future if your plans change.")
 }
 
 
-//If there is no value in savings then this will prompt the user to enter valid data.
-if(savings === ""){
-    prompt("Oops you forgot to write in a number. Please try again.");
-}
-//If there is no value for hotel then this prompt will appear to ensure data is input.
-if(hotel === ""){
-    prompt("Oops you forgot to write in a number. Please try again.");
-}
-//Tests the driving prompt for a response.
-if(driving === ""){
-    prompt("Oops you forgot something. Please enter a valid response.");
-}
-//Tests the flying prompt for valid input
-if(flying === ""){
-    prompt("Oops you forgot something. Please enter a valid response.");
-}
-//Tests the train prompt for input.
-if(train === ""){
-    prompt("Oops you forgot something. Please enter a valid response.");
-}
+
