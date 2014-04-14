b3d45f08e55da0b9928701631efe5d640e2f4a1f
DaveGall <dlaker74@fullsail.edu>
2014-04-14 14:45:22 -0700

    Added a rental car prompt for the train option also as they may not have a car either way.I have also commented the rest of the lines that I left uncommented last time.

--- Gall_Dave_WPF/Conditionals_Personal/js/script.js
+++ Gall_Dave_WPF/Conditionals_Personal/js/script.js
@@ -26,8 +26,13 @@ if(driving === "y" || driving === "Y"){//if the response is yes then do this.
     //Find out which mode of transportation they are taking. Train yes or no.
     var train = prompt("Are you traveling by train? (y) for yes or (n) for no.");
 }if(train === "y" || train === "Y"){//This is the response if you choose to travel by train.
-    var numberOfTrainTickets = prompt("How many train tickets do you need to purchase?");
-    var costPerTrainTicket = prompt("How much does each train ticket cost? \nNo need for any symbols just numbers is good.");
+    var numberOfTrainTickets = prompt("How many train tickets do you need to purchase?");//How many train tickets are needed.
+    var costPerTrainTicket = prompt("How much does each train ticket cost? \nNo need for any symbols just numbers is good.");//Cost of each ticket.
+    var rentalCar = prompt("Will you need a rental car? (y) for yes or (n) for no.");//If a rental car is needed.
+    if(rentalCar === "y" || rentalCar === "Y"){//Yes response for a rental car will produce the code below.
+        var daysOfRental = prompt("How many days will you need the rental car?");//How many days the car is needed.
+        var rentalCost = prompt("How much does the rental car cost per day? \nNo need for signs just a number will work.");//Cost per day of the rental.
+    }
     //The next statement is in case they aren't traveling by train. At this point I have no other options for you sorry.
 }else{
     alert("Sorry I have no other options for you at this time. Please check back in the future if your plans change.")
