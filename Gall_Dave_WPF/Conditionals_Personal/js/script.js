fb114919fb90b3a97569312ba7b2f36199cbaf0b
DaveGall <dlaker74@fullsail.edu>
2014-04-14 13:31:43 -0700

    I have just added a conditional if statement that tells the usee they need to actually enter data. I have done that now so that I don't forget later while I am writing the rest of my code.

--- Gall_Dave_WPF/Conditionals_Personal/js/script.js
+++ Gall_Dave_WPF/Conditionals_Personal/js/script.js
@@ -2,3 +2,9 @@
 //How long can we vacation for?
 //Prompt to ask how much you have saved.
 var savings = prompt("How much money do you have saved? \nJust a number please, you don't need any dollar signs.");
+
+
+//If there is no value in savings then this will prompt the user to enter valid data.
+if(savings === ""){
+    prompt("Oops you forgot to write in a number. Please try again.");
+}
