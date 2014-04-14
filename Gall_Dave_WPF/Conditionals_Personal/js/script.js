1d0e53d4e9ba51255241b900b4051e5db06041ae
DaveGall <dlaker74@fullsail.edu>
2014-04-14 13:27:37 -0700

    First submit of my conditionals personal assignment. I have created the link between the html and the script files and I have written my first prompt. This will be a calculator to see how long of a vacation you can take with the savings you have.

---
+++ Gall_Dave_WPF/Conditionals_Personal/js/script.js
@@ -0,0 +1,4 @@
+//David Gall, 04/14/14, Conditionals_Personal
+//How long can we vacation for?
+//Prompt to ask how much you have saved.
+var savings = prompt("How much money do you have saved? \nJust a number please, you don't need any dollar signs.");
