4a37d08a70310fb0e064104ec1320532680e6c9d
DaveGall <dlaker74@fullsail.edu>
2014-04-15 16:53:24 -0700

    I have added a prompt that will get the day of the week from the user. This is the beginning of my Wack Conditional, depending on what day of the week it is the result will pop up as to what you should do.

--- Gall_Dave_WPF/Conditionals_Wacky/js/script.js
+++ Gall_Dave_WPF/Conditionals_Wacky/js/script.js
@@ -1,2 +1,8 @@
 //David Gall, 04/15/14, Conditionals_Wacky
-
+//Decide what to do on a certain day.
+//The first prompt will get the user to input a day of the week.
+var dayOfWeek = prompt("What day of the week is it today?");
+//This will appear if the user does not make an entry into the day of week prompt.
+if(dayOfWeek === ""){
+    prompt("Oops I think you forgot something. Please make type in a day of the week.");
+}
