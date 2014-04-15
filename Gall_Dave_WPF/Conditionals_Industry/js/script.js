0cdcc322c7304b8d07ec04cf2ceca3e721dadf3e
DaveGall <dlaker74@fullsail.edu>
2014-04-15 12:54:19 -0700

    I have added two prompts to get information from the user. One for the amount of hours needed for the project and another for the number of employees the user has.

--- Gall_Dave_WPF/Conditionals_Industry/js/script.js
+++ Gall_Dave_WPF/Conditionals_Industry/js/script.js
@@ -1,4 +1,12 @@
 //David Gall, 04/15/14, Conditionals_Industry
 //Do you have enough employees if your employees can only work 8 hours on the project?
 //How many more will you need?
-
+var projectHours = Number(prompt("Projected time the project will take to finish in hours."));//This will grab the project hours from the user.
+if(projectHours === ""){
+    prompt("Oops you forgot to input a number.");//If the user doesn't put a value in the box then this message will appear.
+}
+//This next line will ask the user how many employees the company has.
+var employees = Number(prompt("How many employees do you have to work on the project?"));
+if(employees === ""){
+    prompt("Oops you forgot to input a number.");//If the user doesn't put a value then this message will appear.
+}
