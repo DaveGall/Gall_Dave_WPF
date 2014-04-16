28e6c2fbc1bd825653bd72a3f43b2658a6cd392b
DaveGall <dlaker74@fullsail.edu>
2014-04-15 17:11:43 -0700

    I have added a if true for Monday, monday, am, AM, pm and PM. Each choice will result in different displays.

--- Gall_Dave_WPF/Conditionals_Wacky/js/script.js
+++ Gall_Dave_WPF/Conditionals_Wacky/js/script.js
@@ -1,5 +1,5 @@
 //David Gall, 04/15/14, Conditionals_Wacky
-//Decide what to do on a certain day.
+//Decide what to do during summer break.
 //The first prompt will get the user to input a day of the week.
 var dayOfWeek = prompt("What day of the week is it today?");
 //This will appear if the user does not make an entry into the day of week prompt.
@@ -11,4 +11,11 @@ var timeOfDay = prompt("Is it am or pm?");
 //This next if true statement will make sure the user enters a value into the prompt.
 if(timeOfDay === ""){
     prompt("Oops I think you forgot something. Please type in am or pm.");
+}
+//This next section will be if the user chooses monday or Monday and am or AM.
+if((dayOfWeek === "monday" || dayOfWeek === "Monday") && (timeOfDay === "am" || timeOfDay === "AM")){
+    alert("It's Monday AM");
+    //This is the section that will display when the user chooses monday or Monday and PM or pm.
+}if((dayOfWeek === "monday" || dayOfWeek === "Monday") && (timeOfDay === "pm" || timeOfDay ==="PM")){
+    alert("It's Monday PM");
 }
