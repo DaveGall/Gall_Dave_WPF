e82a774a87a5458af6c8289d193a51ff33cc4cf3
DaveGall <dlaker74@fullsail.edu>
2014-04-17 13:48:18 -0700

    Chapter 3 Example 3 secret number. I have just written the var code for the prompt of the secret number.

--- Gall_Dave_WPF/Beginning_Javascript/script.js
+++ Gall_Dave_WPF/Beginning_Javascript/script.js
@@ -1,18 +1,8 @@
+//ch3_example3
+var secretNumber = prompt("Pick a number between 1 and 5;", "");
+secretNumber = parseInt(secretNumber);
 
-var myAge = Number(prompt("Enter your age", " "));
 
-if(myAge >= 0 && myAge <= 10){
-    console.log("myAge is between 0 and 10\n");
-}
-if(!(myAge >= 0 && myAge <= 10)){
-    console.log("myAge is NOT between 0 and 10\n");
-}
-if(myAge >= 80 || myAge <= 10){
-    console.log("myAge is 80 or above OR 10 or below\n");
-}
-if((myAge >= 30 && myAge <= 39) || (myAge >= 80 && myAge <= 89)){
-    console.log("myAge is between 30 and 39 or myAge is between 80 and 89");
-}
 
 
 
