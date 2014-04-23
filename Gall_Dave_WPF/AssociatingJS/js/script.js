8fa9ba8edff34619cfcf90f82a61f7f2dc85f750
DaveGall <dlaker74@fullsail.edu>
2014-04-22 19:32:18 -0700

    Called the function for calcArea to display.

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -3,13 +3,17 @@
 function outptMsg(){
     console.log("Hello World!!");
 }
-
+//Function definition.
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+//Function call.
+calcArea();
+calcArea();
+calcArea();
 
 
 
