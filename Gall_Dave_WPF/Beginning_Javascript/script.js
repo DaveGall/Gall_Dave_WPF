db1855e75ccc054877795c345d5b778e8a169f9c
DaveGall <dlaker74@fullsail.edu>
2014-04-22 19:12:58 -0700

    Chapter 3 Example 5 on how to place a function and call it to action in your code.

--- Gall_Dave_WPF/Beginning_Javascript/script.js
+++ Gall_Dave_WPF/Beginning_Javascript/script.js
@@ -1,22 +1,20 @@
-//ch3_example4
+//ch3_example5
+function convertToCentigrade(degFahren)
+{
+    var degCent;
+    degCent = 5/9 * (degFahren - 32);
 
-var degFahren = new Array(212, "string data", -459.15);
+    return degCent;
+}
+var degFahren = new Array(212, 32, -459.15);
 var degCent = new Array();
 var loopCounter;
 
-for (loopCounter = 0; loopCounter <= 2; loopCounter++) {
-
-    if (isNaN(degFahren[loopCounter])) {
-        alert("Data '" + degFahren[loopCounter] + "' at array index " + loopCounter + " is invalid.");
-        continue;
-    }
-}
-    degCent[loopCounter] = 5/9 * (degFahren[loopCounter] - 32);
-for (loopCounter = 2; loopCounter >= 0; loopCounter--){
-    document.write("Value "+loopCounter+" was "+degFahren[loopCounter]+" degrees Fahrenheit");
-    document.write(" which is "+degCent[loopCounter]+" degrees centigrade<br/>");
+for(loopCounter = 0; loopCounter <=2; loopCounter++){
+    degCent[loopCounter] = convertToCentigrade(degFahren[loopCounter]);
 }
 
-
-
-
+for(loopCounter = 2; loopCounter >=0; loopCounter--){
+    document.write("Value "+loopCounter+ " was "+degFahren[loopCounter]+" degrees Fahrenheit");
+    document.write(" which is "+degCent[loopCounter]+" degrees centigrade<br />");
+}
