5aa482d3044943dfd20d191e799f29ca39e5dc65
DaveGall <dlaker74@fullsail.edu>
2014-04-25 11:35:17 -0700

    I have just added a prompt for getting the value of the radius from the user to pass through the equation.

--- Gall_Dave_WPF/Functions_Worksheet/js/script.js
+++ Gall_Dave_WPF/Functions_Worksheet/js/script.js
@@ -5,6 +5,8 @@
 
 function calcCircumference(){//The beginning of my function.
     var pie = Math.PI;//This is creating a variable that will serve as the value pie. I researched it and found it on the W3Schools website.
+    var radius = Number(prompt("What is the radius of your circle?"));//This will get the radius from the user for the equation.
+    
 
 }
 
