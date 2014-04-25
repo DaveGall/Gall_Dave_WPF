790538ed325b814f85f443186c555fd65a02556a
DaveGall <dlaker74@fullsail.edu>
2014-04-25 11:32:59 -0700

    I have added the beginning of my function and I have placed the first variable between the brackets giving a value for pie.

--- Gall_Dave_WPF/Functions_Worksheet/js/script.js
+++ Gall_Dave_WPF/Functions_Worksheet/js/script.js
@@ -3,5 +3,8 @@
 
 //C = 2*π*r
 
+function calcCircumference(){//The beginning of my function.
+    var pie = Math.PI;//This is creating a variable that will serve as the value pie. I researched it and found it on the W3Schools website.
 
+}
 
