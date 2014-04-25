aba5feeb60f4c99d6db269e5a71e5258763aa096
DaveGall <dlaker74@fullsail.edu>
2014-04-25 11:38:26 -0700

    I have added an equation that will get the value of the circumference of the circle in question.

--- Gall_Dave_WPF/Functions_Worksheet/js/script.js
+++ Gall_Dave_WPF/Functions_Worksheet/js/script.js
@@ -6,6 +6,10 @@
 function calcCircumference(){//The beginning of my function.
     var pie = Math.PI;//This is creating a variable that will serve as the value pie. I researched it and found it on the W3Schools website.
     var radius = Number(prompt("What is the radius of your circle?"));//This will get the radius from the user for the equation.
+    if(radius === ""){
+        prompt("I think you forgot to enter a value!");
+    }//Just in case the user does not enter a value for the radius.
+    var circumference = 2 * pie * radius;//This is the equation to get a value for the circumference of the circle.
     
 
 }
