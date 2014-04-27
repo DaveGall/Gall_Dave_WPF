f1d3e46dc904fa8246688be0b678e3436d2ec5b9
DaveGall <dlaker74@fullsail.edu>
2014-04-27 16:32:15 -0700

    I have added an alert message to let the user know what the purpose is and also added a prompt to get the number of times a user brushes their teeth every day.

--- Gall_Dave_WPF/Functions_Personal/js/script.js
+++ Gall_Dave_WPF/Functions_Personal/js/script.js
@@ -8,4 +8,9 @@ Approximately how many minutes does it take you to floss?
 -Do you rinse with mouthwash?-
 How many times per day do you rinse?
 No need for a prompt for amount of time that takes as we will average it to 1 minute per rinse.
+-?How old are you?-
  */
+
+alert("This little task will determine how long you spend keeping your teeth and your dentist happy each year and throughout your life.");//This is an alert that will let the user know the purpose of the task they are completing.
+
+var brushTeeth = Number(prompt("How many times do you brush your teeth every day?"));//First prompt asking for how many times the user brushes their teeth each day.
