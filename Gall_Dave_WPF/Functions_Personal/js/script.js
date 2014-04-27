07452758788d622544a31b0cbd117f80e984191b
DaveGall <dlaker74@fullsail.edu>
2014-04-27 16:26:06 -0700

    Done with planning out what I want to happen with my code.

--- Gall_Dave_WPF/Functions_Personal/js/script.js
+++ Gall_Dave_WPF/Functions_Personal/js/script.js
@@ -1,8 +1,11 @@
 //David Gall, 04/27/2014, Personal Function Worksheet
+//How many hours are spent keeping the dentist and your teeth happy?
 /*How many times a day do you brush your teeth?
 Approximately how many minutes do spend brushing your teeth?
 -Do You floss your teeth?-
 How many times a day do you floss?
 Approximately how many minutes does it take you to floss?
-
+-Do you rinse with mouthwash?-
+How many times per day do you rinse?
+No need for a prompt for amount of time that takes as we will average it to 1 minute per rinse.
  */
