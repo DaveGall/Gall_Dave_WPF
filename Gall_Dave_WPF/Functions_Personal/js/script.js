3d0d223e2bef1d5f330374a6d02e87938b5d4438
DaveGall <dlaker74@fullsail.edu>
2014-04-27 14:51:16 -0700

    I am writing down the elements of what my code will ask so that I can get an idea of how I will proceed. This code will determine how long you spend in a year taking care of yourself.

--- Gall_Dave_WPF/Functions_Personal/js/script.js
+++ Gall_Dave_WPF/Functions_Personal/js/script.js
@@ -1,1 +1,8 @@
 //David Gall, 04/27/2014, Personal Function Worksheet
+/*How many times a day do you brush your teeth?
+Approximately how many minutes do spend brushing your teeth?
+-Do You floss your teeth?-
+How many times a day do you floss?
+Approximately how many minutes does it take you to floss?
+
+ */
