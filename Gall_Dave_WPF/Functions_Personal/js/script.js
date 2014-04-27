d1ea620187c53374dec462573955c2e7eea87d3a
DaveGall <dlaker74@fullsail.edu>
2014-04-27 16:34:34 -0700

    I have added a prompt to get the length of time a user brushes their teeth each time.

--- Gall_Dave_WPF/Functions_Personal/js/script.js
+++ Gall_Dave_WPF/Functions_Personal/js/script.js
@@ -14,3 +14,4 @@ No need for a prompt for amount of time that takes as we will average it to 1 mi
 alert("This little task will determine how long you spend keeping your teeth and your dentist happy each year and throughout your life.");//This is an alert that will let the user know the purpose of the task they are completing.
 
 var brushTeeth = Number(prompt("How many times do you brush your teeth every day?"));//First prompt asking for how many times the user brushes their teeth each day.
+var lengthBrushing = Number(prompt("Approximately how many minutes do you spend brushing your teeth each time?"));//This is a prompt to get the amount of time the user brushes their teeth each time they brush.
