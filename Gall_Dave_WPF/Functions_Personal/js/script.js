cc0ea55fcdd2ac94ee338afb4d10875945a34a82
DaveGall <dlaker74@fullsail.edu>
2014-04-28 13:06:29 -0700

    I am creating my functions, hopefully they are right. I have tested them and they display the correct results when values are passed through.

--- Gall_Dave_WPF/Functions_Personal/js/script.js
+++ Gall_Dave_WPF/Functions_Personal/js/script.js
@@ -17,10 +17,22 @@ age * 365 - 365 = This will account for the first year of life when you don't br
 if age is > 78 then age * 365 - 365 = This will account for those that are older than 78 and will just return how long they have spent throughout their life on hygiene.
  */
 
+function beforeTime(age){
+    var priorDays = age * 365 - 365;
+    return priorDays;
+    }
+function afterAge(age){
+    var futureDays = (78 - age) * 365;
+    return futureDays;
+}
 alert("This little task will determine how long you spend keeping your teeth and your dentist happy each year and throughout your life.");//This is an alert that will let the user know the purpose of the task they are completing.
 var age = Number(prompt("How old are you? Please use numbers."));//User input for their age to use for determining how long they have been spending on hygiene.
+alert(beforeTime(age));
+alert(afterAge(age));
 var brushTeeth = Number(prompt("How many times do you brush your teeth every day?"));//First prompt asking for how many times the user brushes their teeth each day.
+
 var lengthBrushing = Number(prompt("Approximately how many minutes do you spend brushing your teeth each time?"));//This is a prompt to get the amount of time the user brushes their teeth each time they brush.
+
 var doYouFloss = prompt("Do you floss your teeth? Y or N");//Asking the user if they floss during the day.
 
 if(doYouFloss === "y" || doYouFloss ==="Y"){//If the answer to do you floss is yes this will be the set of commands.
