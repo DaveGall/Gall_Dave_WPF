02724b247f90cf9a5b09bf881eae45cea824f4fe
DaveGall <dlaker74@fullsail.edu>
2014-04-29 14:39:53 -0700

    Switched the price pay and price sell as they were backwards and giving me the wrong results.

--- Gall_Dave_WPF/Functions_Industry/js/script.js
+++ Gall_Dave_WPF/Functions_Industry/js/script.js
@@ -6,16 +6,17 @@ Price of tickets to sell
 Percentage of sale that the company keeps
 */
 
-function profits(quant, sell, pay){//This will determine how many tickets the user needs to sell to make a profit.
-    var ticketSalesNeeded = quant * pay / sell;//Equation for finding the number of tickets the user will need to sell to start making a profit.
+function profits(quant, pay, sell){//This will determine how many tickets the user needs to sell to make a profit.
+    var ticketSalesNeeded = quant * pay / sell + 1;//Equation for finding the number of tickets the user will need to sell to start making a profit.
     return ticketSalesNeeded;//The return value of ticket sales needed to display.
 }
 
 alert("This is a little calculator that will help you figure out how many tickets you will need to sell in order to start making a profit.");
 var quantity = Number(prompt("How many tickets have you purchased to sell?"));//Gets the amount of tickets the user has purchased to sell.
-var priceYouSell = Number(prompt("How much does it cost you to purchase each ticket?"));//Gets the price the user will sell the tickets for.
-var priceYouPay = Number(prompt("How much are you going to sell each ticket for?"));//Gets the price the user pays for each ticket.
+var priceYouPay = Number(prompt("How much does it cost you to purchase each ticket?"));//Gets the price the user pays for each ticket.
+var priceYouSell = Number(prompt("How much do you plan on selling each ticket for each ticket?"));//Gets the price the user will sell the tickets for.
 
+alert(profits(quantity, priceYouPay, priceYouSell));
 
 
 
