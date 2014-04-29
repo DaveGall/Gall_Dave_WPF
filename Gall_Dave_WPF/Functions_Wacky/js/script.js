45123ba2f0178a2a0c47ba90b0166d9fecf8a9a3
DaveGall <dlaker74@fullsail.edu>
2014-04-29 11:13:11 -0700

    This is my final upload for my Wacky Function project. I have added my console.log statement by copying the alert statement I did to test the results. I left the alert statement so both are visible when reading the code and getting a response from the input.

--- Gall_Dave_WPF/Functions_Wacky/js/script.js
+++ Gall_Dave_WPF/Functions_Wacky/js/script.js
@@ -22,16 +22,27 @@ function varietyFish(t, s){//This function will calculate the number of fish per
 }
 alert("This calculator will determine the amount of fish you should stock your new fish tank with. Please use inches when writing in the dimensions of your tank.");//Just a little alert that will let the user know the purpose of this calculator
 var length = Number(prompt("What is the length of your fish tank?"));//Variable for gathering the length of the fish tank.
+if(length === ""){
+    prompt("Please enter a valid response.");
+}
 var width = Number(prompt("What is the width of your aquarium?"));//Variable prompt to get the width from the user.
+if(width === ""){
+    prompt("Please enter a valid response.");
+}
 var height = Number(prompt("What is the height of your fish tank?"));//Variable prompt to get the height of the tank from the user.
+if(height === ""){
+    prompt("Please enter a valid response.");
+}
 var smallFish = 2;//size of small fish that you could put in your tank.
 var mediumFish = 4;//size of medium fish you could put in your tank.
 var largeFish = 6;//size of large fish you could put in your tank.
 var tankGallons = (parseInt(fishTank(length, width, height)));//created a variable using the results from the first function that can be run through the second function for getting the number of fish you can put in the tank.
 var tankThirds = tankGallons * .33;//This variable will divide the tank into thirds for a variety of sizes of fish.
+
+console.log("Your fish tank is approximately "+parseInt(fishTank(length, width, height))+" gallons. \nYou could populate your tank with approximately "+parseInt(numberOfFish(smallFish, tankGallons))+" small fish around 2 inches long. \n"+parseInt(numberOfFish(mediumFish, tankGallons))+" medium fish around 4 inches long. \n"+parseInt(numberOfFish(largeFish, tankGallons))+" large fish around 6 inches long. \nIf you would like a variety of fish then you could populate your tank with; \n"+parseInt(varietyFish(tankThirds, smallFish))+" small fish, "+parseInt(varietyFish(tankThirds, mediumFish))+" medium fish and "+parseInt(varietyFish(tankThirds, largeFish))+" large fish.");//completed statement to display the results to the user.
+
+
 alert("Your fish tank is approximately "+parseInt(fishTank(length, width, height))+" gallons. \nYou could populate your tank with approximately "+parseInt(numberOfFish(smallFish, tankGallons))+" small fish around 2 inches long. \n"+parseInt(numberOfFish(mediumFish, tankGallons))+" medium fish around 4 inches long. \n"+parseInt(numberOfFish(largeFish, tankGallons))+" large fish around 6 inches long. \nIf you would like a variety of fish then you could populate your tank with; \n"+parseInt(varietyFish(tankThirds, smallFish))+" small fish, "+parseInt(varietyFish(tankThirds, mediumFish))+" medium fish and "+parseInt(varietyFish(tankThirds, largeFish))+" large fish.");//completed statement to display the results to the user.
-alert(parseInt(varietyFish(tankThirds, smallFish)));
-alert(parseInt(varietyFish(tankThirds, mediumFish)));
-alert(parseInt(varietyFish(tankThirds, largeFish)));
+
 
 
