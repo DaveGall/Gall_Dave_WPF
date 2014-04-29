d9a856a492177b82e9643b4dad245bd22c8059fa
DaveGall <dlaker74@fullsail.edu>
2014-04-29 14:01:42 -0700

    Starting a function to determine the total profits for the amount of tickets you sell.

--- Gall_Dave_WPF/Functions_Industry/js/script.js
+++ Gall_Dave_WPF/Functions_Industry/js/script.js
@@ -6,8 +6,12 @@ Price of tickets to sell
 Percentage of sale that the company keeps
 */
 
+function profits(quant, pr, per){
+    var yourPercent = 
+}
+
 alert("This is a little calculator that will help you determine your profits for the tickets you sell for a concert or any entertainment event. \nPlease input numbers into the box there is no need to write dollar or percentage signs.");
-var quantityOfTickets = Number(prompt("How many tickets do you have to sell?"));//This will grab the quantity of tickets from the user.
+var quantity = Number(prompt("How many tickets do you have to sell?"));//This will grab the quantity of tickets from the user.
 var price = Number(prompt("How much does each ticket cost?"));//Gets the cost of each ticket from the user.
 var percentage = Number(prompt("What percentage of each ticket do you make? Please enter a whole number."));//This will get the percentage that the user will keep from each ticket.
 
