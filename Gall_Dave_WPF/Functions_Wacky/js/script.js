d8e57077f8c6d20173a291db70a151b4b0273796
DaveGall <dlaker74@fullsail.edu>
2014-04-28 17:45:43 -0700

    I have written down all the note I need and I have started writing my function for my code. I have three parameters that will be input into the function to gain the needed result.

--- Gall_Dave_WPF/Functions_Wacky/js/script.js
+++ Gall_Dave_WPF/Functions_Wacky/js/script.js
@@ -1,2 +1,15 @@
 //David Gall, 04/27/2014, Wacky Functions Worksheet
 
+/*How many fish can you put in your new aquarium. Going on the usual rule for freshwater fish we would assume that you could have 1 inch of fish per gallon of your tank. That's final size of the fish.
+We need to find the volume of the fish tank
+Length of tank * width of tank * height of tank = Volume of the tank. All measurements would need to be in inches
+That answer divided by 231 ( this is how many cubic inches are in a gallon.
+That will give you the number of inches of fish you can have in your tank.
+*/
+//Setting up my function for the code I am writing.
+function fishTank(length, width, height){//These will be my variables that will be run through my function.
+    var volume = length * width * height;//Equation for getting the volume of the users tank.
+    var gallons = volume / 231;//Equation for determining the amount of gallons in the fish tank.
+}
+
+
