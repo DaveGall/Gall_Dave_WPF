d30a5630cddb8c0c7a8835827ec61e1b7804ad90
DaveGall <dlaker74@fullsail.edu>
2014-04-29 13:37:02 -0700

    I have added a comment that describes the purpose of what my code will accomplish.

--- Gall_Dave_WPF/Functions_Industry/js/script.js
+++ Gall_Dave_WPF/Functions_Industry/js/script.js
@@ -1,2 +1,4 @@
-//David Gall, 04/27/2014, Industry Function problem
+//David Gall, 04/29/2014, Industry Function problem
 
+/*This will have to do with the entertainment industry which is in the same category as the industry I am studying for. It will have to do with how much a company will make if they sell a certain amount of tickets at e certain percentage.
+*/
