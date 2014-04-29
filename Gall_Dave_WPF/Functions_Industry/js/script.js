b15f97d8955c5d1940f957fe070cc3db07397232
DaveGall <dlaker74@fullsail.edu>
2014-04-29 13:51:52 -0700

    Added a cost prompt to get the cost of each ticket from the user.

--- Gall_Dave_WPF/Functions_Industry/js/script.js
+++ Gall_Dave_WPF/Functions_Industry/js/script.js
@@ -7,4 +7,10 @@ Percentage of sale that the company keeps
 */
 
 alert("This is a little calculator that will help you determine your profits for the tickets you sell for a concert or any entertainment event. \nPlease input numbers into the box there is no need to write dollar or percentage signs.");
-var numberOfTickets = Number(prompt("How many tickets do you have to sell?"));//This will grab the quantity of tickets from the user.
+var quantityOfTickets = Number(prompt("How many tickets do you have to sell?"));//This will grab the quantity of tickets from the user.
+var price = Number(prompt("How much does each ticket cost?"));//Gets the cost of each ticket from the user.
+
+
+
+
+
