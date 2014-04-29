4ad0d7ca63289c6ccdb7f1137034814d7ebe0bf6
DaveGall <dlaker74@fullsail.edu>
2014-04-29 13:43:35 -0700

    Added a variable prompt to get the number of tickets the company has to sell.

--- Gall_Dave_WPF/Functions_Industry/js/script.js
+++ Gall_Dave_WPF/Functions_Industry/js/script.js
@@ -1,7 +1,10 @@
 //David Gall, 04/29/2014, Industry Function problem
 
 /*This will have to do with the entertainment industry which is in the same category as the industry I am studying for. It will have to do with how much a company will make if they sell a certain amount of tickets at e certain percentage.
+Quantity of tickets to sell
+Price of tickets to sell
+Percentage of sale that the company keeps
 */
 
 alert("This is a little calculator that will help you determine your profits for the tickets you sell for a concert or any entertainment event. \nPlease input numbers into the box there is no need to write dollar or percentage signs.");
-
+var numberOfTickets = Number(prompt("How many tickets do you have to sell?"));//This will grab the quantity of tickets from the user.
