289f17a2e6490b75b373b3f0266674d7a634e278
DaveGall <dlaker74@fullsail.edu>
2014-04-29 13:40:06 -0700

    Added an alert that will display for the user to see and describes what the calculator will do for them.

--- Gall_Dave_WPF/Functions_Industry/js/script.js
+++ Gall_Dave_WPF/Functions_Industry/js/script.js
@@ -2,3 +2,6 @@
 
 /*This will have to do with the entertainment industry which is in the same category as the industry I am studying for. It will have to do with how much a company will make if they sell a certain amount of tickets at e certain percentage.
 */
+
+alert("This is a little calculator that will help you determine your profits for the tickets you sell for a concert or any entertainment event. \nPlease input numbers into the box there is no need to write dollar or percentage signs.");
+
