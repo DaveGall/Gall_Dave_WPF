31e456988928823549421470887a830fbf516cd5
DaveGall <dlaker74@fullsail.edu>
2014-04-29 18:27:33 -0700

    While loop screencast completed.

--- Gall_Dave_WPF/AssociatingJS/js/script.js
+++ Gall_Dave_WPF/AssociatingJS/js/script.js
@@ -1,6 +1,13 @@
-// Functions - Anonymous Functions
+// Functions - Loops
 
+//While loop
 
+var b = 10;//sets up the index
+
+while(b > 0){//checks the condition
+    console.log(b+" kegs on the wall.");
+    b--;//increments or decrements the index
+}
 
 
 
